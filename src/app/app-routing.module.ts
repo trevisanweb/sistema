c927514cd37be919d58bd7686fe5a43392a87ed1
Felipe Trevisan <cadastro.felipetrevisan@gmail.com>
2018-11-01 17:33:35 -0300

    Route Login Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { AuthGuard } from './auth-guard.service';
 import {
   NbAuthComponent,
   NbLoginComponent,
@@ -13,7 +14,9 @@ import {
 } from '@nebular/auth';
 
 const routes: Routes = [
-  { path: 'pages', loadChildren: 'app/pages/pages.module#PagesModule' },
+  { path: 'pages',
+    canActivate: [AuthGuard],
+    loadChildren: 'app/pages/pages.module#PagesModule' },
   {
     path: 'auth',
     component: NbAuthComponent,
