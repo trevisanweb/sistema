01719374df5b52582ec901e78b55662ae5bd2b8b
Felipe Trevisan <cadastro.felipetrevisan@gmail.com>
2019-01-15 16:23:57 -0200

    Adjust Auth Update

---
+++ src/app/@theme/components/auth/models/user.d.ts
@@ -0,0 +1,11 @@
+export declare class NbUser {
+    id?: number;
+    identifier?: string;
+    email?: string;
+    password?: string;
+    rememberMe?: boolean;
+    terms?: boolean;
+    confirmPassword?: string;
+    fullName?: string;
+    constructor(id?: number, email?: string, identifier?: string, password?: string, rememberMe?: boolean, terms?: boolean, confirmPassword?: string, fullName?: string);
+}
---
+++ src/app/@theme/components/auth/models/user.js
@@ -0,0 +1,15 @@
+var NbUser = /** @class */ (function () {
+    function NbUser(id, email, password, rememberMe, terms, confirmPassword, fullName) {
+        this.id = id;
+        this.email = email;
+        this.identifier = identifier;
+        this.password = password;
+        this.rememberMe = rememberMe;
+        this.terms = terms;
+        this.confirmPassword = confirmPassword;
+        this.fullName = fullName;
+    }
+    return NbUser;
+}());
+export { NbUser };
+//# sourceMappingURL=user.js.map
