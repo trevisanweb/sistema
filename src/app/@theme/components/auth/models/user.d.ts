37a7ac762c5b011e4a70b9c5d8a18ff29f6c55d1
Felipe Trevisan <cadastro.felipetrevisan@gmail.com>
2019-01-16 18:17:40 -0200

    Finish Login/Register

--- src/app/@theme/components/auth/models/user.d.ts
+++ src/app/@theme/components/auth/models/user.d.ts
@@ -5,6 +5,6 @@ export declare class NbUser {
     rememberMe?: boolean;
     terms?: boolean;
     confirmPassword?: string;
-    fullName?: string;
-    constructor(id?: number, email?: string, password?: string, rememberMe?: boolean, terms?: boolean, confirmPassword?: string, fullName?: string);
+    username?: string;
+    constructor(id?: number, email?: string, password?: string, rememberMe?: boolean, terms?: boolean, confirmPassword?: string, username?: string);
 }
--- src/app/@theme/components/auth/models/user.js
+++ src/app/@theme/components/auth/models/user.js
@@ -1,12 +1,12 @@
 var NbUser = /** @class */ (function () {
-    function NbUser(id, email, password, rememberMe, terms, confirmPassword, fullName) {
+    function NbUser(id, email, password, rememberMe, terms, confirmPassword, username) {
         this.id = id;
         this.email = email;
         this.password = password;
         this.rememberMe = rememberMe;
         this.terms = terms;
         this.confirmPassword = confirmPassword;
-        this.fullName = fullName;
+        this.username = username;
     }
     return NbUser;
 }());
